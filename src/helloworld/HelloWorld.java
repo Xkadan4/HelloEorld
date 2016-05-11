405ddd9e885d2c85469c3cf8611ddb300220c455
david.kadanik <david.kadanik@A211-09.pslib.cz>
2016-05-11 13:07:40 +0200

    Merge cesky
    Conflicts:
    	src/helloworld/HelloWorld.java
    
    převzata všechna hlášení

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,7 +15,9 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*české hlášení*/
         System.out.println("Ahoj světe!");
+        //anglické hlášení
         System.out.println("HelloWorld");
         
         
